9f3a409a823f40eb51aded7dc9045bd90041908f
Tony Lo <hongyau369@gmail.com>
2023-10-18 21:55:24 -0400

    Removed unnecessary files, updated landing page to intro and link to linkedin/github

---
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -0,0 +1,14 @@
+// import './MenuList.css';
+// import MenuListItem from '../MenuListItem/MenuListItem';
+import './PortfolioPage.css'
+
+export default function PortfolioPage() {
+  return (
+    <>
+        <h1>Welcome to my portfolio!</h1>
+        <div>My name is Tony Lo, and I'm excited to have you here. While I'm diligently working on putting the finishing touches on my website, you can still explore my work and learn more about my professional journey. To stay connected and updated on my progress, feel free to connect with me on LinkedIn or explore my coding projects on GitHub. Just click on the links below to visit my profiles and let's connect!"</div>
+        <div><a href='https://www.linkedin.com/in/tonyhongyaulo/' target='_blank'>LinkedIn</a></div>
+        <div><a target='_blank' href='https://github.com/tonyhylo'>Github</a></div>
+    </>
+  );
+}
