96a250e20b04bee13c814155513194112f9d5039
Tony Lo <hongyau369@gmail.com>
2023-11-11 08:02:22 -0500

    Added contact form feature that uses emailjs

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -3,9 +3,39 @@
 import "./PortfolioPage.css";
 import { useState, useEffect, useContext } from "react";
 import { MouseContext } from "../../context/mouse-context";
+import { useForm } from "react-hook-form";
+import emailjs from '@emailjs/browser';
 
 export default function PortfolioPage() {
   const { cursorType, cursorChangeHandler } = useContext(MouseContext);
+
+  const [contactMessage, setContactMessage] = useState({
+    name: "name",
+    email: "email@domain",
+    message: "message",
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    emailjs.sendForm("service_4vndmxf", "template_5eu99da", e.target, "0XBORLU5d3HUCENLn")
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset()
+  };
+
+  // function handleSubmit(evt) {
+  //   evt.preventDefault();
+  //   console.log(contactMessage);
+  // }
+
+  function handleChange(evt) {
+    setContactMessage({...contactMessage, [evt.target.name]: evt.target.value })
+  }
+
+
   return (
     <>
       <main id="home">
@@ -255,82 +285,93 @@ export default function PortfolioPage() {
         <div id="contact"></div>
       </main>
       <div class="hidden-footer">
-        <div>
-          <h1>Contact</h1>
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-person-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-            </svg>
-            Name
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="Your name"
-            onMouseEnter={() => cursorChangeHandler("hovered")}
-            onMouseLeave={() => cursorChangeHandler("")}
-            onClick={() => cursorChangeHandler("clicked")}
-            onMouseOut={() => cursorChangeHandler("hovered")}
-          ></input>
-          <label for="exampleFormControlInput1" class="form-label">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-envelope-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-            </svg>
-            Email Address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="email@example.com"
-            onMouseEnter={() => cursorChangeHandler("hovered")}
-            onMouseLeave={() => cursorChangeHandler("")}
-            onClick={() => cursorChangeHandler("clicked")}
-            onMouseOut={() => cursorChangeHandler("hovered")}
-          ></input>
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-chat-left-text-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z" />
-            </svg>
-            Message
-          </label>
-          <textarea
-            class="form-control"
-            id="exampleFormControlTextarea1"
-            placeholder="Hi Tony ..."
-            rows="3"
-            onMouseEnter={() => cursorChangeHandler("hovered")}
-            onMouseLeave={() => cursorChangeHandler("")}
-            onClick={() => cursorChangeHandler("clicked")}
-            onMouseOut={() => cursorChangeHandler("hovered")}
-          ></textarea>
-        </div>
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div>
+            <h1>Contact</h1>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-person-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+              </svg>
+              Name
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleFormControlInput1"
+              name="from-name"
+              placeholder="Your name"
+              onChange={handleChange}
+              onMouseEnter={() => cursorChangeHandler("hovered")}
+              onMouseLeave={() => cursorChangeHandler("")}
+              onClick={() => cursorChangeHandler("clicked")}
+              onMouseOut={() => cursorChangeHandler("hovered")}
+            ></input>
+            <label for="exampleFormControlInput1" class="form-label">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-envelope-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+              </svg>
+              Email Address
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="email@example.com"
+              name="from-email"
+              onChange={handleChange}
+              onMouseEnter={() => cursorChangeHandler("hovered")}
+              onMouseLeave={() => cursorChangeHandler("")}
+              onClick={() => cursorChangeHandler("clicked")}
+              onMouseOut={() => cursorChangeHandler("hovered")}
+            ></input>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-chat-left-text-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z" />
+              </svg>
+              Message
+            </label>
+            <textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              placeholder="Hi Tony ..."
+              name="from-message"
+              onChange={handleChange}
+              rows="3"
+              onMouseEnter={() => cursorChangeHandler("hovered")}
+              onMouseLeave={() => cursorChangeHandler("")}
+              onClick={() => cursorChangeHandler("clicked")}
+              onMouseOut={() => cursorChangeHandler("hovered")}
+            ></textarea>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
         <footer class="tl-footer">
           <div class="icons-footer">
             <a
